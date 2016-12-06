1f8475eba2637aa1b8b33eeb5accf363895bccbc
ЧернышеваАИ <svch9039@mail.ru>
2016-12-06 15:47:20 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+angular.module("App",[]).controller("Todo", function($scope)
+                                   {
+  $scope.todo = [{name:"Сдать долги"},
+                 {name:"Погладить кота"},
+                 {name:"Поспать"},
+                 {name:"Захватить мир"}];
+  $scope.archive = [];
+  $scope.Add = function()
+  {
+    $scope.todo.push({name:$scope.newItem});
+    $scope.newItem = "";
+  }
+  $scope.Clear = function()
+  {
+    for(var i=0;i<$scope.todo.length;i++)
+      {
+        if ($scope.todo[i].done)
+          {
+            $scope.archive.push($scope.todo[i]);
+            $scope.todo.splice(i,1);
+            i--;
+          }
+      }
+  }
+});
